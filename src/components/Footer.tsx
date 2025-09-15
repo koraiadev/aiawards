92cbf740e787473db0bc3106b31356a77253a319
koraiadev <youngdev@koraia.org>
2025-09-15 16:08:50 +0900

    refactor: footer 로고 임시 주석 처리

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,16 +2,16 @@
 
 export default function Footer() {
     return (
-        <footer className="border-t">
+        <footer className="border-t border-gray-50/20">
             <section className="mx-auto max-w-[var(--w-section-full)] w-full">
                 <div className="h-60 flex justify-center items-center flex-col gap-2">
                     <div className="mb-12 flex justify-center items-center gap-10">
-                        <a href="https://it.chosun.com" target="_blank" rel="noopener noreferrer">
-                            <img src="/img/logo/logo-it-chosun.png" alt="" />
-                        </a>
-                        <a href="https://koraia.org/" target="_blank" rel="noopener noreferrer">
-                            <img src="/img/logo/logo-koraia.png" alt="" />
-                        </a>
+                        {/*<a href="https://it.chosun.com" target="_blank" rel="noopener noreferrer">*/}
+                        {/*    <img src="/img/logo/logo-it-chosun.png" alt="" />*/}
+                        {/*</a>*/}
+                        {/*<a href="https://koraia.org/" target="_blank" rel="noopener noreferrer">*/}
+                        {/*    <img src="/img/logo/logo-koraia.png" alt="" />*/}
+                        {/*</a>*/}
                     </div>
                     <p>Copyright © 2025 All rights reserved</p>
                     <p>대한민국 인공지능대상 운영사무국 & (사)한국인공지능협회</p>
