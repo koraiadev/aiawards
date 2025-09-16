bbf39db1e36771091d7212daeafc58e94d0ba832
koraiadev <youngdev@koraia.org>
2025-09-16 18:47:31 +0900

    tmp deploy

---
+++ _next/static/yVddxy3iP1_TpnSYeikl3/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/41dfc0c0f02b3e1b.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
